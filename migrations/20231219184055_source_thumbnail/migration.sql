32a844dac30d9fba9ca54ff2a95c16d9f0898b24
SnO2WMaN <me@sno2wman.net>
2023-12-20 03:41:09 +0900

    source thumbnail

---
+++ migrations/20231219184055_source_thumbnail/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "BilibiliMADSource" ADD COLUMN     "thumbnailUrl" TEXT;
+
+-- AlterTable
+ALTER TABLE "NicovideoVideoSource" ADD COLUMN     "thumbnailUrl" TEXT;
+
+-- AlterTable
+ALTER TABLE "SoundcloudVideoSource" ADD COLUMN     "thumbnailUrl" TEXT;
+
+-- AlterTable
+ALTER TABLE "YoutubeVideoSource" ADD COLUMN     "thumbnailUrl" TEXT;
